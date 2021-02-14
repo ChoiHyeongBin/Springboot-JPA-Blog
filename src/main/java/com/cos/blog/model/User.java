3a290c6bc6361a22eb3030ef115b25f484d23d20
ChoiHyeongBin <chekqhksha@gmail.com>
2021-02-14 21:03:10 +0900

    Board, Reply Entity Add

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -11,7 +11,16 @@ import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
-// ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder	// 빌더 패턴
+//ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity		// User 클래스가 MySQL 에 테이블이 생성이 됨
 public class User {
 	
