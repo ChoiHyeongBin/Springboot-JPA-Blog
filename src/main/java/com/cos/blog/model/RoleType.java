b67b260f2bd1a352bb21491a3d7d2ffe70c9ded8
ChoiHyeongBin <chekqhksha@gmail.com>
2021-02-16 23:50:17 +0900

    UserRepository Interface create and paging test

---
+++ src/main/java/com/cos/blog/model/RoleType.java
@@ -0,0 +1,6 @@
+package com.cos.blog.model;
+
+// 데이터의 도메인(범위)을 만들 때 사용하는 enum
+public enum RoleType {
+	USER, ADMIN
+}
--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -4,12 +4,15 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,8 +23,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder	// 빌더 패턴
-//ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
+// ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity		// User 클래스가 MySQL 에 테이블이 생성이 됨
+// @DynamicInsert		// insert 시에 null 인 필드를 제외시켜줌
 public class User {
 	
 	@Id		// Primary key
@@ -36,10 +40,12 @@ public class User {
 	
 	@Column(nullable = false, length = 50)
 	private String email;
+
+	// @ColumnDefault("user")
+	// DB 는 RoleType 이라는 게 없음
+	@Enumerated(EnumType.STRING)
+	private RoleType role;		// Enum 을 쓰는게 좋음 // ADMIN, USER
 	
-	@ColumnDefault("'user'")
-	private String role;		// Enum 을 쓰는게 좋음 // admin, user, manager
-	
-	@CreationTimestamp		// 시간이 자동 입력
+	@CreationTimestamp		// 시간이 자동 입력 (현재 시간을 createDate 에 넣어줌)
 	private Timestamp createDate;
 }
