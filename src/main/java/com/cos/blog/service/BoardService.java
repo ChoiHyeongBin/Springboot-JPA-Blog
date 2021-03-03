182563c384cde85a6ab51c45b0a22c17aa57dbc2
ChoiHyeongBin <chekqhksha@gmail.com>
2021-03-03 23:37:46 +0900

    board update add

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -40,5 +40,16 @@ public class BoardService {
 	public void boardDelete(int id) {
 		boardRepository.deleteById(id);
 	}
+	
+	@Transactional
+	public void boardUpdate(int id, Board requestBoard) {
+		Board board = boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 찾기 실패 : 아이디를 찾을 수 없습니다.");
+				});	// 영속화 완료
+		board.setTitle(requestBoard.getTitle());
+		board.setContent(requestBoard.getContent());
+		// 해당 함수로 종료 시(Service 가 종료될 때) 트랜잭션이 종료됨. 이때 더티체킹 - 자동 업데이트가 됨. DB flush
+	}
 
 }
